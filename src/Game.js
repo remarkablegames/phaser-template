b1228bf3eccf4e994df7c463f6f9da3b5a32b468
Mark <mark@remarkablemark.org>
2018-01-06 15:15:09 -0500

    feat(states): create and start game with Boot state
    https://phaser.io/news/2015/06/using-states-tutorial

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,12 @@
 import Phaser from 'phaser';
+import { Boot } from './states';
 
 export default class Game extends Phaser.Game {
   constructor() {
     // width, height, renderer, parent
     super(800, 600, Phaser.AUTO, document.getElementById('game'));
+
+    this.state.add(Boot.key, Boot, false);
+    this.state.start(Boot.key);
   }
 }
---
+++ src/states/Boot.js
@@ -0,0 +1,5 @@
+import { State } from 'phaser';
+
+export default class Boot extends State {
+  static key = 'boot';
+}
