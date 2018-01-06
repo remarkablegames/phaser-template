56a7db47f839cb74541ccfc0c16137ac6d72ca37
Mark <mark@remarkablemark.org>
2018-01-06 18:25:41 -0500

    feat(sprites): update physics for Ground sprite
    Tutorial:
    https://phaser.io/tutorials/making-your-first-phaser-game/part3

--- src/sprites/Ground.js
+++ src/sprites/Ground.js
@@ -4,4 +4,22 @@ const spriteKey = 'ground';
 
 export default class Ground extends Sprite {
   static key = spriteKey;
+
+  /**
+   * @param {Phaser.Game} game
+   * @param {Number}      x
+   * @param {Number}      y
+   */
+  constructor(game, x, y) {
+    super(game, x, y, spriteKey);
+
+    // Add sprite to the game.
+    game.add.existing(this);
+
+    // Enable physics for sprite.
+    game.physics.arcade.enable(this);
+
+    // This stops the sprite from falling away when you jump on it.
+    this.body.immovable = true;
+  }
 }
