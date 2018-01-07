117f0db9e7e0e868d0e200f9146ed8065a561a6a
Mark <mark@remarkablemark.org>
2018-01-06 19:04:27 -0500

    feat(sprites): add sprite Sky to existing game when instantiated

--- src/sprites/Sky.js
+++ src/sprites/Sky.js
@@ -4,4 +4,16 @@ const spriteKey = 'sky';
 
 export default class Sky extends Sprite {
   static key = spriteKey;
+
+  /**
+   * @param {Phaser.Game} game
+   * @param {Number}      x
+   * @param {Number}      y
+   */
+  constructor(game, x, y) {
+    super(game, x, y, spriteKey);
+
+    // Add sprite to the game.
+    game.add.existing(this);
+  }
 }
