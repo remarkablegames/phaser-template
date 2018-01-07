63f87e9c1dee4db8f4d4de810869df76feb597a9
Mark <mark@remarkablemark.org>
2018-01-06 20:08:39 -0500

    feat(sprites): control the Dude with the keyboard (cursor keys)
    If up key is down and player is touching platform:
    - Move player up (jump)
    
    If left key is down:
    - Move to the left (walk)
    - Play left animation
    
    If right key is down:
    - Move to the right (walk)
    - Play right animation
    
    Otherwise:
    - Stop movement
    - Show still frame
    
    Tutorial:
    https://phaser.io/tutorials/making-your-first-phaser-game/part6

--- src/sprites/Dude.js
+++ src/sprites/Dude.js
@@ -30,10 +30,40 @@ export default class Dude extends Sprite {
     // Our two animations, walking left and right.
     this.animations.add(leftAnimationKey, [0, 1, 2, 3], 10, true);
     this.animations.add(rightAnimationKey, [5, 6, 7, 8], 10, true);
+
+    // Create cursor keys for movement.
+    this.cursors = game.input.keyboard.createCursorKeys();
   }
 
   update() {
+    const { animations, body, cursors } = this;
+
     // Collide the player with the platform or else the player will fall through.
-    this.game.physics.arcade.collide(this, groups.platforms);
+    const hitPlatform = this.game.physics.arcade.collide(
+      this,
+      groups.platforms
+    );
+
+    // Reset the player velocity (movement).
+    body.velocity.x = 0;
+
+    if (cursors.left.isDown) {
+      // Move to the left.
+      body.velocity.x = -150;
+      animations.play(leftAnimationKey);
+    } else if (cursors.right.isDown) {
+      // Move to the right.
+      body.velocity.x = 150;
+      animations.play(rightAnimationKey);
+    } else {
+      // Stand still.
+      animations.stop();
+      this.frame = 4;
+    }
+
+    // Allow player to jump if sprite is touching the ground.
+    if (cursors.up.isDown && body.touching.down && hitPlatform) {
+      body.velocity.y = -350;
+    }
   }
 }
