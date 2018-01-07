e2bae9d73594293d9c5d8fec39d64a6ebe5e3e6b
Mark <mark@remarkablemark.org>
2018-01-06 20:48:28 -0500

    feat(sprites): update Star with physics and checks in update loop
    Star sprite has arcade physics enabled along with properties.
    
    In the sprite `update` lifecycle, check for collision against
    platforms group (so it doesn't fall through) and overlap with the
    player sprite (so it gets removed from screen).
    
    Tutorial:
    https://phaser.io/tutorials/making-your-first-phaser-game/part7

--- src/sprites/Star.js
+++ src/sprites/Star.js
@@ -1,4 +1,5 @@
 import { Sprite } from 'phaser';
+import { groups, sprites } from '../shared';
 
 const spriteKey = 'star';
 
@@ -15,5 +16,36 @@ export default class Star extends Sprite {
 
     // Add sprite to the game.
     game.add.existing(this);
+
+    // Enable physics for sprite.
+    game.physics.arcade.enable(this);
+
+    // Let gravity do its thing.
+    this.body.gravity.y = 100;
+
+    // This just gives each star a slightly random bounce value.
+    this.body.bounce.y = 0.4 + Math.random() * 0.2;
+  }
+
+  collectStar(star, player) {
+    // Removes the star from the screen.
+    star.kill();
+  }
+
+  update() {
+    const { game } = this;
+
+    // Check for collision against the platforms so the stars won't fall
+    // through to the bottom of the game.
+    game.physics.arcade.collide(this, groups.platforms);
+
+    // Check for an overlap between the player and star.
+    game.physics.arcade.overlap(
+      this,
+      sprites.player,
+      this.collectStar,
+      null,
+      this
+    );
   }
 }
