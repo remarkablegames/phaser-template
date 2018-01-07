d727dda02e01e4cb2b9a434ad6a6d8d3b76b7cf1
Mark <mark@remarkablemark.org>
2018-01-06 19:55:27 -0500

    feat(sprites): give Dude physics properties and animations
    Tutorial:
    https://phaser.io/tutorials/making-your-first-phaser-game/part4

--- src/sprites/Dude.js
+++ src/sprites/Dude.js
@@ -1,7 +1,33 @@
 import { Sprite } from 'phaser';
 
 const spriteKey = 'dude';
+const leftAnimationKey = 'left';
+const rightAnimationKey = 'right';
 
 export default class Dude extends Sprite {
   static key = spriteKey;
+
+  /**
+   * @param {Phaser.Game} game
+   * @param {Number}      x
+   * @param {Number}      y
+   */
+  constructor(game, x, y) {
+    super(game, x, y, spriteKey);
+
+    // Add sprite to the game.
+    game.add.existing(this);
+
+    // We need to enables physics on the player.
+    game.physics.arcade.enable(this);
+
+    // Player physics properties. Give the little guy a slight bounce.
+    this.body.bounce.y = 0.2;
+    this.body.gravity.y = 300;
+    this.body.collideWorldBounds = true;
+
+    // Our two animations, walking left and right.
+    this.animations.add(leftAnimationKey, [0, 1, 2, 3], 10, true);
+    this.animations.add(rightAnimationKey, [5, 6, 7, 8], 10, true);
+  }
 }
