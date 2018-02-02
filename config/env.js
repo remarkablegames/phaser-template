eb13ee87439558631928502645d927d5ad589f06
Mark <mark@remarkablemark.org>
2018-02-01 22:46:17 -0500

    chore(config): change custom environment variable prefix to `APP_`
    Replace the convention that comes from create-react-app in which
    custom environment variables begin with `REACT_APP_`.

--- config/env.js
+++ config/env.js
@@ -49,13 +49,13 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
   .map(folder => path.resolve(appDirectory, folder))
   .join(path.delimiter);
 
-// Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
+// Grab NODE_ENV and APP_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in Webpack configuration.
-const REACT_APP = /^REACT_APP_/i;
+const APP = /^APP_/i;
 
 function getClientEnvironment(publicUrl) {
   const raw = Object.keys(process.env)
-    .filter(key => REACT_APP.test(key))
+    .filter(key => APP.test(key))
     .reduce(
       (env, key) => {
         env[key] = process.env[key];
