5ed51a767604d5ff8bc6874580bd8382235d5054
Mark <mark@remarkablemark.org>
2018-02-05 00:15:13 -0500

    feat(states): set scale mode to show entire game given proportions

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -1,10 +1,11 @@
-import { State } from 'phaser';
+import { ScaleManager, State } from 'phaser';
 import Load from './Load';
 
 export default class Boot extends State {
   static key = 'boot';
 
   init() {
+    this.scale.scaleMode = ScaleManager.SHOW_ALL;
     this.state.start(Load.key);
   }
 }
