790e5405566a5a43afeddeb8aae50aff4ccfc88b
Mark <mark@remarkablemark.org>
2018-02-07 21:22:26 -0500

    refactor(states): tidy state Main

--- src/states/Main.js
+++ src/states/Main.js
@@ -7,7 +7,7 @@ export default class Main extends State {
   static key = 'main';
 
   create() {
-    const { game } = this;
+    const { add, game } = this;
 
     // We're going to be using physics, so enable the Arcade Physics system.
     this.physics.startSystem(Physics.ARCADE);
@@ -18,10 +18,10 @@ export default class Main extends State {
     // The platforms group contains the ground and the 2 ledges we can jump on.
     // It's created after the background so the order of layers (z-depth) is
     // maintained (otherwise, the platforms will be hidden by the background).
-    groups.platforms = this.add.group();
+    groups.platforms = add.group();
 
     // Here we create the ground.
-    const ground = new Ground(this.game, 0, this.world.height - 64);
+    const ground = new Ground(game, 0, this.world.height - 64);
 
     // Scale it to fit the width of the game.
     // (The original sprite is 400x32 in size.)
@@ -36,7 +36,7 @@ export default class Main extends State {
     sprites.player = new Dude(game, 32, this.world.height - 150);
 
     // Create stars group.
-    groups.stars = this.add.group();
+    groups.stars = add.group();
 
     // Here we'll create 12 of them evenly spaced apart.
     for (let i = 0; i < 12; i++) {
