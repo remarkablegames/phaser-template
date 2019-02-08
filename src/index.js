c3e6fa6cf4363d765866535e96054d225e6b5330
Mark <mark@remarkablemark.org>
2019-02-07 19:30:10 -0500

    feat(index): set backgroundColor to black and text color to white

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ registerServiceWorker();
  * @see {@link https://photonstorm.github.io/phaser3-docs/global.html#GameConfig}
  */
 const config = {
+  backgroundColor: '#000',
   width: 800,
   height: 600,
   title: 'Phaser Template',
