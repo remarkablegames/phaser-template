8c71d2dae37fda17bb868ba5b2fdcc404927ddda
Mark <mark@remarkablemark.org>
2019-11-17 01:04:12 -0500

    feat(scripts): add `deploy.sh` and update npm script `deploy`
    Source:
    https://github.com/remarkablemark/web-app-template/commit/29759b5

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ensure working index is clean
+stash_output=$(git add . && git stash)
+
+# run lint
+npm run lint
+
+# remove `build` folder
+npm run clean
+
+# build production artifacts
+npm run build
+
+# deploy `build` contents to remote branch `gh-pages`
+if [[ $(command -v npx) ]]; then
+  npx --no-install gitploy build gh-pages
+else
+  $(npm bin)/gitploy build gh-pages
+fi
+
+# return working index to initial state (if applicable)
+if [[ $stash_output != 'No local changes to save' ]]; then
+  git stash pop
+fi
