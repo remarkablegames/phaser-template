17fcc0326c8a9a7f42531dd7507312a1427e2dc0
Mark <mark@remarkablemark.org>
2021-01-26 21:12:44 -0500

    chore(src): remove serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './index.css';
 import { Boot, Main } from './scenes';
-// import * as serviceWorker from './serviceWorker';
 
 const { Game, Scale } = window.Phaser;
 
@@ -31,8 +30,3 @@ const config = {
 };
 
 new Game(config);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
