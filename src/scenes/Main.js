d763efb0db5358bb1ea4b9b537ef7bd096fb352c
Mark <mark@remarkablemark.org>
2021-01-27 00:20:28 -0500

    refactor(scenes): undo destructuring in Main create

--- src/scenes/Main.js
+++ src/scenes/Main.js
@@ -10,27 +10,20 @@ export default class Main extends Scene {
   }
 
   create() {
-    const {
-      game: {
-        config: { height },
-      },
-      physics,
-    } = this;
-
     // A simple background for our game.
     this.add.image(0, 0, TEXTURES.SKY).setOrigin(0);
 
     // The platforms group contains the ground and the 2 ledges we can jump on.
     // It's created after the background so the order of layers (z-depth) is
     // maintained (otherwise, the platforms will be hidden by the background).
-    const platforms = physics.add.staticGroup({
+    const platforms = this.physics.add.staticGroup({
       defaultKey: TEXTURES.GROUND,
     });
 
     // Create the ground (scale it to fit the widt of the game and scale it to
     // fit the width of the game). The original sprite is 400x32 in size.
     platforms
-      .get(0, height - 64)
+      .get(0, this.game.config.height - 64)
       .setOrigin(0)
       .setScale(2)
       .refreshBody();
@@ -45,7 +38,7 @@ export default class Main extends Scene {
     });
 
     // Create stars group.
-    const stars = physics.add.group({
+    const stars = this.physics.add.group({
       classType: Star,
     });
 
@@ -55,17 +48,17 @@ export default class Main extends Scene {
     }
 
     // Collide the star with the platform or else the star will fall through.
-    physics.add.collider(stars, platforms);
+    this.physics.add.collider(stars, platforms);
 
     // Create player.
-    const player = new Player(this, 32, height - 150).init();
+    const player = new Player(this, 32, this.game.config.height - 150).init();
     this.player = player;
 
     // Collide the player with the platform or else the player will fall through.
-    physics.add.collider(player, platforms);
+    this.physics.add.collider(player, platforms);
 
     // Check for overlap between the player and the star.
-    physics.add.overlap(player, stars, this.collectStar, null, this);
+    this.physics.add.overlap(player, stars, this.collectStar, null, this);
 
     // Display score.
     this.score = 0;
