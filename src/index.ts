324641642c0538de6730adf305583b4c3f1811a9
Mark <mark@remarkablemark.org>
2022-01-26 22:02:05 -0500

    refactor: tidy index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 import './style.css';
 
-import { Game, Scale } from 'phaser';
+import Phaser from 'phaser';
 
-import { Boot, Main } from './scenes';
+import * as scenes from './scenes';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 /**
  * https://photonstorm.github.io/phaser3-docs/Phaser.Types.Core.html#.GameConfig
  */
-const config = {
+new Phaser.Game({
   width: 800, // 1024
   height: 600, // 768
   title: 'Phaser Template',
   url: 'https://remarkablegames.org/phaser-template/',
   version: process.env.VERSION,
-  scene: [Boot, Main],
+  scene: Object.values(scenes),
   physics: {
     default: 'arcade',
     arcade: {
@@ -28,9 +28,7 @@ const config = {
   disableContextMenu: isProduction,
   backgroundColor: '#fff',
   scale: {
-    mode: Scale.FIT,
-    autoCenter: Scale.CENTER_BOTH,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-};
-
-new Game(config);
+});
