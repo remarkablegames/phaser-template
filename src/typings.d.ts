a3c669ae4bc5ac1fe4c9e6050b9a2ed0ed422fb7
Mark <mark@remarkablemark.org>
2022-02-17 00:02:25 -0500

    chore: add audio extensions to typings.d.ts

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -45,3 +45,18 @@ declare module '*.svg' {
   const src: string;
   export default src;
 }
+
+declare module '*.ogg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.mp3' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.wav' {
+  const src: string;
+  export default src;
+}
