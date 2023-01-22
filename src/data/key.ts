f320b371f7fe7080969bf1e7b2610c9178a9e143
Mark <mark@remarkablemark.org>
2023-01-22 18:25:04 -0500

    refactor(data): make key readonly

--- src/data/key.ts
+++ src/data/key.ts
@@ -3,14 +3,14 @@ const image = {
   ground: 'ground',
   sky: 'sky',
   star: 'star',
-};
+} as const;
 
 const scene = {
   boot: 'boot',
   main: 'main',
-};
+} as const;
 
 export const key = {
   image,
   scene,
-};
+} as const;
