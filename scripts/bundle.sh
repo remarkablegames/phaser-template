b5197782b07ea5f9da209657dba88beeede7c44f
Mark <mark@remarkablemark.org>
2024-02-26 22:13:44 -0500

    ci(github): upload to itch.io in workflow release-please.yml

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -3,14 +3,16 @@
 BUNDLE=true npm run build
 
 # replace absolute URLs with relative URLs
-if [ "$(uname)" == 'Darwin' ]; then
+if [[ $(uname) == 'Darwin' ]]; then
   sed -i '' -e 's|src="/|src="|g' -e 's|href="/|href="|g' dist/index.html
 else
   sed -i -e 's|src="/|src="|g' -e 's|href="/|href="|g' dist/index.html
 fi
 
 # zip file
-zip -r "dist/$(npm pkg get name | tr -d \")-$(npm pkg get version | tr -d \").zip" dist
-echo
-find dist -type f -depth 1 -name '*.zip'
-open dist
+if [[ $CI != 'true' ]]; then
+  zip -r "dist/$(npm pkg get name | tr -d \")-$(npm pkg get version | tr -d \").zip" dist
+  echo
+  find dist -type f -depth 1 -name '*.zip'
+  open dist
+fi
