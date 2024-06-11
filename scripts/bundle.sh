8e5395e7b86407b83a9906c0cad62ed7039c57c0
Mark <remarkablemark@users.noreply.github.com>
2024-06-10 22:06:23 -0400

    build(scripts): update bundle.sh

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,11 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
+# build bundle
 BUNDLE=true npm run build -- --base=./
 
 # zip file
+zip -r "dist/$(npm pkg get name | tr -d \")-$(npm pkg get version | tr -d \").zip" dist
+echo
+find dist -type f -name '*.zip'
+
+# open folder
 if [[ $CI != 'true' ]]; then
-  zip -r "dist/$(npm pkg get name | tr -d \")-$(npm pkg get version | tr -d \").zip" dist
-  echo
-  find dist -type f -depth 1 -name '*.zip'
   open dist
 fi
