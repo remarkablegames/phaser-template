c437f888e28790d84727de37188750851d37e9d1
Mark <mark@remarkablemark.org>
2024-08-15 20:30:55 -0400

    chore(eslint): migrate to eslint 9
    https://eslint.org/docs/latest/use/configure/migration-guide

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  includeIgnoreFile(gitignorePath),
+
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ),
+
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier,
+      'simple-import-sort': simpleImportSort,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    rules: {
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'no-console': 'error',
+      'no-debugger': 'error',
+      'prettier/prettier': 'error',
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': 'error',
+    },
+  },
+];
