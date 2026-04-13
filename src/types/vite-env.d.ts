dc60b2405e77c71aedae5b219a539f8b53ce727b
Mark <mark@remarkablemark.org>
2026-04-12 20:36:19 -0400

    refactor(types): move vite types from vite-env.d.ts to tsconfig.json

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,68 +1,6 @@
-/// <reference types="vite/client" />
-
 interface ImportMetaEnv {
-  readonly DEV: boolean;
   readonly VITE_APP_HOMEPAGE: string;
   readonly VITE_APP_NAME: string;
   readonly VITE_APP_VERSION: string;
   readonly VITE_GOOGLE_ANALYTICS_ID: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
-declare module '*.avif' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.bmp' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.gif' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.jpg' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.jpeg' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.png' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.webp' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.svg' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.ogg' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.mp3' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.wav' {
-  const src: string;
-  export default src;
 }
